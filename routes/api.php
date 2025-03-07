9c803e60622a3cd12ef0b780e6779152a1b543dc
LionelMv <mvangicode@gmail.com>
2025-03-07 21:32:11 +0300

    feat: Add CRUD for tasks
    Added endpoints for create, update, get and delete tasks.
    Created Task Policies for each task with their user.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
+// Auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Tasks
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/tasks', [TaskController::class, 'store']); // Create
+    Route::get('/tasks', [TaskController::class, 'index']); // List
+    Route::put('/tasks/{task}', [TaskController::class, 'update']); // Update
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']); // Delete
+});
